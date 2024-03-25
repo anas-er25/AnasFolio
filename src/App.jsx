8f501b72cf80de41da648bd2e9654fe4fae75577
Anas ER-RAKIBI <anaserrakibi@gmail.com>
2024-03-25 16:07:07 +0000

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Navbar/>
       <div className="container">
         <Hero/>
-        
+        <Skills/>
       </div>
     </>
   )
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,7 +1,40 @@
+import { SKILLS } from "../../utils/data";
 import "./Skills.css";
+import { useState } from "react";
+import SkillInfoCard from "./SkillInfoCard/SkillInfoCard";
+import SkillCard from "./SkillCard/SkillCard";
 
 const Skills = () => {
-  return <></>;
+  const [SelectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+  const handleSelectedSkill = (data) => {
+    setSelectedSkill(data);
+  };
+  return (
+    <>
+      <section className="skills-container">
+        <h5>Technical Proficiency</h5>
+        <div className="skills-content">
+          <div className="skills">
+            {SKILLS.map((skill) => (
+              <SkillCard
+                key={skill.title}
+                title={skill.title}
+                icon={skill.icon}
+                isActive={SelectedSkill.title === skill.title}
+                onClick={() => handleSelectedSkill(skill)}
+              />
+            ))}
+          </div>
+          <div className="skills-info">
+            <SkillInfoCard
+              heading={SelectedSkill.title}
+              skills={SelectedSkill.skills}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Skills;
