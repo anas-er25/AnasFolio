650049434cb3a206b06ac2bb8c05f3784ad826f1
Anas ER-RAKIBI <anaserrakibi@gmail.com>
2024-03-26 11:05:44 +0000

    experience section

--- src/Components/Experiences/Experience.jsx
+++ src/Components/Experiences/Experience.jsx
@@ -3,6 +3,7 @@ import { EXPERIENCES } from "../../utils/data";
 import "./Experience.css";
 import ExperienceCard from "./ExperienceCard/ExperienceCard";
 import Slider from "react-slick";
+
 const Experience = () => {
   const sliderRef = useRef();
   const settings = {
@@ -22,15 +23,45 @@ const Experience = () => {
       },
     ],
   };
+
+  const handleNext = () => {
+    sliderRef.current.slickNext();
+  }
+
+  const handlePrev = () => {
+    sliderRef.current.slickPrev();
+  }
+
   return (
     <>
       <section className="experience-container">
         <h5>Experience</h5>
         <div className="experience-content">
-          <Slider ref={sliderRef}{...settings}>
-          {EXPERIENCES.map((experience) => (
-            <ExperienceCard key={experience.title} details={experience} />
-          ))}
+          <div className="arrow-right" onClick={handleNext}>
+          <span className="material-symbols-outlined">
+          <svg width={20} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#74C0FC" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+          </span>
+          </div>
+
+          <div className="arrow-left" onClick={handlePrev}>
+            <span className="material-symbols-outlined">
+            <svg
+              width={20}
+              height={20}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+            >
+              <path
+                fill="#74C0FC"
+                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+              />
+            </svg>
+            </span>
+          </div>
+          <Slider ref={sliderRef} {...settings}>
+            {EXPERIENCES.map((experience) => (
+              <ExperienceCard key={experience.title} details={experience} />
+            ))}
           </Slider>
         </div>
       </section>
